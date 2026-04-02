6b8ca1baf1d3f3cb2dd7f75c72caf153e6fe839f
rizvireza <rizvi.reza@gmail.com>
2026-04-02 12:49:32 -0400

    create db

--- globo/Api/Program.cs
+++ globo/Api/Program.cs
@@ -1,9 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<HouseDbContext>(options =>
+    options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking));
 
 var app = builder.Build();
 
@@ -16,27 +21,12 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
+app.MapGet("/houses", (HouseDbContext dbContext) =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+    return dbContext.Houses.ToList();
+});
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
-app.Run();
+    app.Run();
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
--- globo/Api/README.md
+++ globo/Api/README.md
@@ -1,4 +1,16 @@
+# Building an End-to-end SPA Using ASP.NET Core Web API and React
+https://github.com/RolandGuijt/ps-globomantics-webapi-react
+
 # Web API
 dotnet new webapi -minimal
 dotnet build
 dotnet run
+
+dotnet add package Microsoft.EntityFrameworkCore.Sqlite --version 8.0.0
+## For working with migrations
+dotnet add package Microsoft.EntityFrameworkCore.Design --version 8.0.0
+
+## create migration with name initial
+dotnet ef migrations add initial
+dotnet ef database update
+
