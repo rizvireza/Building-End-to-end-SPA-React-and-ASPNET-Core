ee95ddaf569cb2995f2a6c5c2fca6c3e3fca4c91
rizvireza <rizvi.reza@gmail.com>
2026-04-23 13:52:08 -0400

    House Detail

--- globo/ReactWeb/src/house/HouseDetail.tsx
+++ globo/ReactWeb/src/house/HouseDetail.tsx
@@ -1,4 +1,15 @@
+import { useParams } from "react-router-dom";
+import { useFetchHouse } from "../hooks/HouseHooks";
+import ApiStatus from "../apiStatus";
+
 const HouseDetail = () =>{
+    const {id} = useParams();
+    if (!id) throw new Error("House id is not found.");
+    const houseId = parseInt(id);
+    const {data, status, isSuccess} = useFetchHouse(houseId);
+    if(!isSuccess) return <ApiStatus status={status} />;
+    if(!data) return <div>House not found</div>
+    
     return (<></>);
 }
 
